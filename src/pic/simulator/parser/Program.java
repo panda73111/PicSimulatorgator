de831469cfefbfd8deb1640bc86e0dce45bac152
LorToso <LorToso@gmail.com>
2014-04-04 19:05:13 +0200

    Added some minor (lets just call it functionallity) to the Program-class

--- src/pic/simulator/parser/Program.java
+++ src/pic/simulator/parser/Program.java
@@ -1,22 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package pic.simulator.parser;
 
-/**
- *
- * @author hudini
- */
+import java.util.ArrayList;
+
 public class Program {
 
-    public int length() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    public void loadFromFile(String filename)
+	private ArrayList<Command> program;
+	
+	public Program()
+	{	
+		program = new ArrayList<>();
+	}
+	public Program(String filename)
+	{
+		this();
+		loadProgramFromFile(filename);
+	}
+	
+	////////////////////////////////////////////////////////////////////////
+	
+    public void loadProgramFromFile(String filename)
     {
+		throw new UnsupportedOperationException("Not supported yet."); // To
+																		// change
+																		// body
+																		// of
+																		// generated
+																		// methods,
+																		// choose
+																		// Tools
+																		// |
+																		// Templates.
+	}
+    
+    ////////////////////////////////////////////////////////////////////////
+    
+    public int length() {
+    	return program.size();
+    }
+    public Command getCommand(int cmdIndex)
+    {
+    	if(cmdIndex >= program.size())
+    		return null;
     	
+    	return program.get(cmdIndex);
     }
 }
