a6446879147e28b0751a91a34c57a0a0b03bda6d
panda73111 <sebastian.huether@gmx.de>
2014-04-08 10:28:01 +0200

    Bugfix: reading the input file line by line +Debug output: Instruction and line number

--- src/pic/simulator/parser/Program.java
+++ src/pic/simulator/parser/Program.java
@@ -5,47 +5,56 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Program {
+public class Program
+{
 
 	private ArrayList<Command> program;
-	
-	public Program()
-	{	
+
+	public Program() {
 		program = new ArrayList<>();
 	}
-	public Program(String filename) throws IOException
-	{
+
+	public Program(String filename) throws IOException {
 		this();
 		loadProgramFromFile(filename);
 	}
-	
-	////////////////////////////////////////////////////////////////////////
-	
-    public void loadProgramFromFile(String filename) throws IOException
-    {
-    	BufferedReader br = new BufferedReader(new FileReader(filename));
-        try {
-            String line = br.readLine();
-            while(line!=null)
-            {
-            	Command cmd = Command.newInstance(line);
-            	program.add(cmd);
-            }
-        } finally {
-            br.close();
-        }
+
+	// //////////////////////////////////////////////////////////////////////
+
+	public void loadProgramFromFile(String filename) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		try
+		{
+			String line;
+			while ((line = br.readLine()) != null)
+			{
+				if (line.startsWith(" ") || line.length() < 9)
+					continue;
+
+				short lineNumber = Short.valueOf(line.substring(0, 4), 16);
+				short opcode = Short.valueOf(line.substring(5, 9), 16);
+
+				System.out.printf("line %4d - opcode 0x%04X\n", lineNumber, opcode);
+
+				Command cmd = Command.newInstance(opcode);
+				program.add(cmd);
+			}
+		} finally
+		{
+			br.close();
+		}
 	}
-    
-    ////////////////////////////////////////////////////////////////////////
-    
-    public int length() {
-    	return program.size();
-    }
-    public Command getCommand(int cmdIndex)
-    {
-    	if(cmdIndex >= program.size())
-    		return null;
-    	
-    	return program.get(cmdIndex);
-    }
+
+	// //////////////////////////////////////////////////////////////////////
+
+	public int length() {
+		return program.size();
+	}
+
+	public Command getCommand(int cmdIndex) {
+		if (cmdIndex >= program.size())
+			return null;
+
+		return program.get(cmdIndex);
+	}
 }
