e336b9d6ede32b4686c619f6fb14c4f749e895f0
panda73111 <sebastian.huether@gmx.de>
2014-05-13 14:08:36 +0200

    Added reset() to InterruptionHandler, so processor reset() doesn't need to instantiate a new InterruptionHander

--- src/pic/simulator/InterruptionHandler.java
+++ src/pic/simulator/InterruptionHandler.java
@@ -4,26 +4,35 @@ import java.util.PriorityQueue;
 
 import pic.simulator.interrupts.Interruption;
 
-public class InterruptionHandler 
+public class InterruptionHandler
 {
-	final Processor myProcessor;
-	final PriorityQueue<Interruption> interruptionQueue;
-	
-	public InterruptionHandler(Processor proc)
-	{
-		myProcessor = proc;
-		interruptionQueue = new PriorityQueue<>();
-	}
-	
-	public Interruption getInterruption()
-	{
-		return interruptionQueue.poll();
-	}
-	
-	public void causeInterruption(int interruptionID)
-	{
-		Interruption interrupt = new Interruption(myProcessor, interruptionID);
-		interruptionQueue.add(interrupt);
-	}
+    final Processor                   myProcessor;
+    final PriorityQueue<Interruption> interruptionQueue;
 
+    public InterruptionHandler(Processor proc)
+    {
+        myProcessor = proc;
+        interruptionQueue = new PriorityQueue<>();
+    }
+
+    public boolean hasInterruption()
+    {
+        return !interruptionQueue.isEmpty();
+    }
+
+    public Interruption getInterruption()
+    {
+        return interruptionQueue.poll();
+    }
+
+    public void causeInterruption(int interruptionID)
+    {
+        Interruption interrupt = new Interruption(myProcessor, interruptionID);
+        interruptionQueue.add(interrupt);
+    }
+
+    public void reset()
+    {
+        interruptionQueue.clear();
+    }
 }
